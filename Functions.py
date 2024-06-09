7db7134f1f169f7fbdb3d6a7058942752be2602e
MeisterYM <yannick_mart@yahoo.com>
2024-06-09 16:55:13 -0500

    Added Functions

---
+++ Functions.py
@@ -0,0 +1,18 @@
+print("Sup with it.");
+
+#Function for saying hello.
+def greet():
+    print("Not much. How you?");
+
+#Calling the hello function.
+greet();
+
+#Function for squaring a number.
+def square(x):
+    result = x * x;
+    return result;
+
+#Calling the square function within a string.
+print("Not much. Just found out 2 squared is", square(2));
+
+print("Uh...cool.");
